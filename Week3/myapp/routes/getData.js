987bb44e8e16e6d5c3676d30a46f44a5e1668be0
魏士弘 <stone@weishihongde-MacBook-Pro.local>
2019-01-28 01:29:03 +0800

    change var number to type Number by *1 ; conditional added: isNaN() because you can not use == NaN.

--- Week3/myapp/routes/getData.js
+++ Week3/myapp/routes/getData.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const number = req.query.number;
+  var number = req.query.number;
+  number = number * 1;
   function isEmpty(obj) {
     for(let key in obj) {
       if(obj.hasOwnProperty(key))
@@ -10,22 +11,28 @@ router.get('/', (req, res) => {
       }
     return true;
   }
+  console.log('req.query= ' + req.query);
+  console.log('req.query.number= ' + number);
+  console.log(typeof(number));
+  console.log(number);
+
   if (isEmpty(req.query)){
     res.send('<h2>Lack of Parameter</h2>');
     }
-  else if ( parseInt(number) == NaN ) {
-      res.render('<h2>Wrong Parameter</h2>');
+  else if ( isNaN(number) ) {
+    res.send('<h2>Wrong Parameter</h2>');
     }
   else {
       function getDataFunc(argInput){
         var dataSum = 0 ;
-        for (let i = 0; i < (argInput + 1)/10 ; i += 1){
+        for (let i = 0; i < argInput + 1 ; i += 1){
           dataSum += i ;
         }
         return dataSum;
         }
       var result = new Object(getDataFunc(number));
       res.send(result);
+
       }
 })
 module.exports = router;
